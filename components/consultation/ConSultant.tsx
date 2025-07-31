a405d2a010f35574a02c279b42d16475ed8e9c89
Osama Ayub <ayubosama416@gmail.com>
2025-07-31 10:09:17 +0500

    made the content center aligned on tablet and mobile devices

--- components/consultation/ConSultant.tsx
+++ components/consultation/ConSultant.tsx
@@ -19,7 +19,7 @@ const ConSultant = () => {
       </div>
 
       {/* Foreground Content */}
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 w-full relative z-10 px-4 md:px-6">
+      <div className="grid grid-cols-1 lg:grid-cols-2 md:items-center gap-12 w-full relative z-10 px-4 md:px-6">
 
         {/* Left Side: Heading + CTA */}
         <div className="flex flex-col justify-center">
